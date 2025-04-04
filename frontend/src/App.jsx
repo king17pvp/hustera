96e782645287144024a91c8989d6c2fc7576eeb7
DangNH225478 <Dang.NH225478@sis.hust.edu.vn>
2025-04-04 08:19:59 +0700

    finished backend for course listing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -187,8 +187,8 @@ const sampleStats = [
 
 const App = () => {
 
+  // const { isAuthenticated } = useSelector((state) => state.auth);
   const { isAuthenticated } = useSelector((state) => state.auth);
-
 
   return (
     <>
