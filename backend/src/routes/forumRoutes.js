b692f44ae53c4eaf3172b4fac51a27e6ce0738d2
King17pvp <tuanhai1304@gmail.com>
2025-04-13 02:23:07 +0700

    Modified forum backend

--- backend/src/routes/forumRoutes.js
+++ backend/src/routes/forumRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const forumController = require('../controllers/forumController');
 
-router.get('/forum/:threadId', forumController.getForumOnClick);
-router.post('/forum/:threadId/answers', forumController.postAnswer);
+router.get('/', forumController.getForum);
+router.get('/filters', forumController.getFilters); // 🔥 
+router.get('/:threadId', forumController.getForumOnClick);
+router.post('/:threadId/answers', forumController.postAnswer);
 
 module.exports = router;
