79d4cf5129499c0e2391ee64c444a1c5a00bd4be
quan-0811 <quan.nh081104@gmail.com>
2025-05-08 07:32:45 +0700

    i aint doing this anymore. peace

--- frontend/src/pages/ForumSingle.jsx
+++ frontend/src/pages/ForumSingle.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
 import { useParams, useLocation } from "react-router-dom";
-
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Breadcrumb from "../components/BreadCrumb";
